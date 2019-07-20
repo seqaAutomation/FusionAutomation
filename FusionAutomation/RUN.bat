2aadf1d21304bdf789abcc29c7fd36686c92a229
ejijo <ejijo@ejijo-in.oradev.oraclecorp.com>
2019-07-20 14:35:59 +0530

    Bat

--- FusionAutomation/RUN.bat
+++ FusionAutomation/RUN.bat
@@ -1,6 +1,3 @@
-set projectLocation=D:\GitTrial\test\FusionAutomation
-cd %projectLocation%
-
-set classpath=%projectLocation%\bin;%projectLocation%\lib\*
-
-java -Durl="test" org.testng.TestNG %projectLocation%\testng.xml
+SET ANT_HOME=D:\Personal\Jars\apache-ant-1.9.14-bin\apache-ant-1.9.14
+SET PATH=%PATH%;%ANT_HOME%\bin
+ant compile
